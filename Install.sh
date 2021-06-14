2a564aab87cf6dff3e251a60bc7491f7389485e1
Daniil <32302524+Bazhyk@users.noreply.github.com>
2021-06-14 19:51:24 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -11,6 +11,8 @@ pwd
 git clone https://github.com/Bazhyk/1.git
 #Place for run tests
 #sleep 60m (optional, depends on test time) 
-aws s3 cp ./1/Install.sh s3://bazhyk777/
+aws s3 cp ./1/"EXAMPLE.LOG" s3://bazhyk777/
+aws ec2 stop-instances --instance-ids ${aws_instance.webserver.id}
+
 
 
